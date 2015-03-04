8c54a868c225a64b0ed66716b37d1e3bddf518b0
1ambda <1amb4a@gmail.com>
2015-03-04 19:55:06 +0900

    change port 3000 -> 9000

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,7 +35,7 @@ module.exports = function (grunt) {
     },
     express: {
       options: {
-        port: process.env.PORT || 9000
+        port: process.env.PORT || 3000
       },
       dev: {
         options: {
@@ -200,7 +200,7 @@ module.exports = function (grunt) {
         options: {
           nodeArgs: ['--debug-brk'],
           env: {
-            PORT: process.env.PORT || 9000
+            PORT: process.env.PORT || 3000
           },
           callback: function (nodemon) {
             nodemon.on('log', function (event) {
--- server/config/local.env.sample.js
+++ server/config/local.env.sample.js
@@ -6,7 +6,7 @@
 // You will need to set these on the server you deploy to.
 
 module.exports = {
-  DOMAIN:           'http://localhost:9000',
+  DOMAIN:           'http://localhost:3000',
   SESSION_SECRET:   'trendbuilding-secret',
 
   // Control debug level for modules using visionmedia/debug
