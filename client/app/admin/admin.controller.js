ce9ef4bb6896f990d6d88f09f2169b80d2e15f6e
1ambda <1amb4a@gmail.com>
2015-03-07 20:43:46 +0900

    add modal in admin.html

--- client/app/admin/admin.controller.js
+++ client/app/admin/admin.controller.js
@@ -1,17 +1,17 @@
 'use strict';
 
 angular.module('buildingApp')
-  .controller('AdminCtrl', function ($scope, $http, Auth, User) {
+  .controller('AdminCtrl', function ($scope, $http, Auth, User, Modal) {
 
     // Use the User $resource to fetch all users
     $scope.users = User.query();
 
-    $scope.delete = function(user) {
+    $scope.delete = Modal.confirm.delete(function(user) { // callback when modal is confirmed
       User.remove({ id: user._id });
       angular.forEach($scope.users, function(u, i) {
         if (u === user) {
           $scope.users.splice(i, 1);
         }
       });
-    };
+    });
   });
