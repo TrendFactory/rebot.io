b567bbe99c227c67a533b20cdb87f62a4893fe87
1ambda <1amb4a@gmail.com>
2015-03-10 23:12:48 +0900

    working linkedin connection component

--- client/app/account/settings/settings.controller.js
+++ client/app/account/settings/settings.controller.js
@@ -4,18 +4,20 @@ angular.module('buildingApp')
   .controller('SettingsCtrl', function ($scope, User, Auth) {
     $scope.errors = {};
 
+    console.log($scope.user);
+
     $scope.changePassword = function(form) {
       $scope.submitted = true;
       if(form.$valid) {
         Auth.changePassword( $scope.user.oldPassword, $scope.user.newPassword )
-        .then( function() {
-          $scope.message = 'Password successfully changed.';
-        })
-        .catch( function() {
-          form.password.$setValidity('mongoose', false);
-          $scope.errors.other = 'Incorrect password';
-          $scope.message = '';
-        });
+          .then( function() {
+            $scope.message = 'Password successfully changed.';
+          })
+          .catch( function() {
+            form.password.$setValidity('mongoose', false);
+            $scope.errors.other = 'Incorrect password';
+            $scope.message = '';
+          });
       }
-		};
+    };
   });
